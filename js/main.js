fd83c378a14b109713da092f1a9fb42dc5e26fe4
Ringo <ringong95@gmail.com>
2016-10-17 14:00:16 -0700

    Added fonts, and text transistion, fixed heapbox interaction

--- js/main.js
+++ js/main.js
@@ -1,8 +1,10 @@
 $( document ).ready(function() {
-	$('#dropdown').heapbox()
 
-	$('#dropdown').heapbox().change(function(){
-		console.log("hope")
+	$('#dropdown').heapbox({'onChange':function(){
+		$('.main').addClass('shrunkheader');
+		$('.loader').css('display','flex');	
+		$('#dropdown option[value=selction').remove();
+		$("#dropdown").heapbox("update"); //Updates heapbox to fit the change i just made to the selection it is based on
 		var input = $('select').val()
 		var url = 'https://api.nytimes.com/svc/topstories/v2/'+input+'.json';
 		$('.article-section').empty();
@@ -15,9 +17,10 @@ $( document ).ready(function() {
 		})
 
 		.done(function(data){
-			$('.main').addClass('shrunkheader');
+
+			
 			var htmlinstert ='<li><a href=';
-			var htmlmiddle = '><div class=article style=background:url(';
+			var htmlmiddle = '><div class=article style=background-image:url(';
 			var htmlinsterend = '</div></a></li>';
 			var info = [];
 			info = $(data.results).filter(function(key,value){
@@ -28,11 +31,18 @@ $( document ).ready(function() {
 				var image = value.multimedia[4].url;
 				var url = value.url;
 				var text = value.abstract;
-				var abstract = ')><div class="abstractpart"><p>'
+				var abstract = ')><div class="abstractpart"><div class="gradient"><p>'
 
 
-				$('.article-section').append(htmlinstert+url+htmlmiddle+image+abstract+text+'</p></div>'+htmlinsterend);
+				$('.article-section').append(htmlinstert+url+htmlmiddle+image+abstract+text+'</p></div></div>'+htmlinsterend);
 			});
+			$('.article-wrapper').slideDown(1000)
+		})
+
+		.always(function(){
+		$('.loader').css('display','none')	
 		});
-	});
+
+	}
+});
 });	
