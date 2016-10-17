a7b2132faefd300e8845d3e3ec689cfbe1fed7ff
Ringo <ringong95@gmail.com>
2016-10-16 17:12:31 -0700

    adding word transistion and header relocation

--- js/main.js
+++ js/main.js
@@ -1,41 +1,35 @@
+$( document ).ready(function() {
+	$('#dropdown').change(function(){
+		var input = $('select').val()
+		var url = 'https://api.nytimes.com/svc/topstories/v2/'+input+'.json';
+		$('.article-section').empty();
+		url += '?' + $.param({
+			'api-key': "2cee7ea7b62f4e7d83a741c409dae5e4"
+		});
+		$.ajax({
+			url: url,
+			method: 'GET',
+		})
+
+		.done(function(data){
+			$('.main').addClass('shrunkheader');
+			var htmlinstert ='<li><a href=';
+			var htmlmiddle = '><div class=article style=background:url(';
+			var htmlinsterend = '</div></a></li>';
+			var info = [];
+			info = $(data.results).filter(function(key,value){
+				return $(value.multimedia).length>=5;
+			})
+			info.splice (12);
+			$.each(info, function (key,value) {
+				var image = value.multimedia[4].url;
+				var url = value.url;
+				var text = value.abstract;
+				var abstract = ')><div class="abstractpart"><p>'
 
 
-$("#dropdown").change(function(){
-	$('.article-section').empty();
-	var input = $('select').val()
-	var url = 'https://api.nytimes.com/svc/topstories/v2/'+input+'.json';
-	url += '?' + $.param({
-		'api-key': "2cee7ea7b62f4e7d83a741c409dae5e4"
-	});
-	// console.log(url)
-	$.ajax({
-		url: url,
-		method: 'GET',
-	})
-// comments
-.done(function(data){
-	var htmladdition = "<div class='article' style='background:url(";
-	var htmlendaddition = "')'</a></div>";
-	var info = [];
-
-	info = $(data.results).filter(function(key,value){
-
-		return $(value.multimedia).length>=5;
-
-	})
-	
-	info.splice (12);
-	console.log(info)
-	
-	$.each(info, function(key,value){
-		var image = value.multimedia[4].url;
-		var text = value.abstract
-		$('.article-section').append(htmladdition+image+"'><p>"+text+htmlendaddition);
-				// $('.article-insertion').append("<p>"+text +"</p></div>");
-
+				$('.article-section').append(htmlinstert+url+htmlmiddle+image+abstract+text+'</p></div>'+htmlinsterend);
 			});
-});
-
-
-
-});
+		});
+	});
+});	
