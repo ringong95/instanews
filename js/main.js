07fd0dac12aec12de421e9a8f29687d7d2a73b11
Ringo <ringong95@gmail.com>
2016-10-17 17:23:50 -0700

    finished stretch goals

--- js/main.js
+++ js/main.js
@@ -1,13 +1,15 @@
 $( document ).ready(function() {
 
 	$('#dropdown').heapbox({'onChange':function(){
+		var curheight = 
 		$('.main').addClass('shrunkheader');
-		$('.loader').css('display','flex');	
+		$('.loader').show();	
 		$('#dropdown option[value=selction').remove();
 		$("#dropdown").heapbox("update"); //Updates heapbox to fit the change i just made to the selection it is based on
 		var input = $('select').val()
 		var url = 'https://api.nytimes.com/svc/topstories/v2/'+input+'.json';
 		$('.article-section').empty();
+		$('.article-wrapper').hide();
 		url += '?' + $.param({
 			'api-key': "2cee7ea7b62f4e7d83a741c409dae5e4"
 		});
@@ -19,28 +21,29 @@ $( document ).ready(function() {
 		.done(function(data){
 
 			
-			var htmlinstert ='<li><a href=';
-			var htmlmiddle = '><div class=article style=background-image:url(';
+			
+			var htmlmiddle = ;
 			var htmlinsterend = '</div></a></li>';
 			var info = [];
 			info = $(data.results).filter(function(key,value){
 				return $(value.multimedia).length>=5;
 			})
 			info.splice (12);
+
+
 			$.each(info, function (key,value) {
-				var image = value.multimedia[4].url;
-				var url = value.url;
-				var text = value.abstract;
-				var abstract = ')><div class="abstractpart"><div class="gradient"><p>'
+				var url = '<li><a href='+value.url+">";
+				var image = '<div class=article style=background-image:url('+value.multimedia[4].url+">";
+				var text = ')><div class="abstractpart"><div class="gradient"><p>'+value.abstract+'</p></div></div></a></li>';
 
 
-				$('.article-section').append(htmlinstert+url+htmlmiddle+image+abstract+text+'</p></div></div>'+htmlinsterend);
+				$('.article-section').append(url+image+text);
 			});
 			$('.article-wrapper').slideDown(1000)
 		})
 
 		.always(function(){
-		$('.loader').css('display','none')	
+			$('.loader').hide();	
 		});
 
 	}
