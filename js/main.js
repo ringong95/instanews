2ebc9fbffc0ce2bf9f3c2c4bfb6e957c3342e657
Ringo <ringong95@gmail.com>
2016-10-25 14:23:21 -0700

    Fixed commit errors

--- js/main.js
+++ js/main.js
@@ -6,8 +6,8 @@ $( document ).ready(function() {
 			$('.loader').show();	
 			$('#dropdown option[value=selction').remove();// this removes the selection option from the html dom thing
 			$("#dropdown").heapbox("update"); //Updates heapbox to fit the change i just made to the selection it is based on the html dom and effect the dom dom
-			let input = $('select').val()
-			let url = 'https://api.nytimes.com/svc/topstories/v2/'+input+'.json';
+			var input = $('select').val()
+			var url = 'https://api.nytimes.com/svc/topstories/v2/'+input+'.json';
 			$('.article-section').empty();
 			$('.article-wrapper').hide();
 			url += '?' + $.param({
@@ -20,8 +20,8 @@ $( document ).ready(function() {
 
 			.done(function(data){
 
-			let info = [];
-			// the filter function here lets it dig through the array and keep only the hinted ones and put that into the letrible Info
+			var info = [];
+			// the filter function here lets it dig through the array and keep only the hinted ones and put that into the varrible Info
 			info = $(data.results).filter(function(key,value){
 				return $(value.multimedia).length>=5;
 				// it "returns" back only ones that fulfill that requirement
@@ -29,16 +29,16 @@ $( document ).ready(function() {
 			info.splice (12);
 			// this i over thought so much it just cuts out everything past the 12th one
 
-			let article-insertion =""
+			var articleinsertion = ""
 			$.each(info, function (key,value) {
-				article-insertion += '<li><a href='+value.url+">";
-				article-insertion += '<div class=article style=background-image:url('+value.multimedia[4].url+">";
-				article-insertion += ')><div class="abstractpart"><div class="gradient"><p>'+value.abstract+'</p></div></div></a></li>';
+				articleinsertion += '<li><a href='+value.url+">";
+				articleinsertion += '<div class=article style=background-image:url('+value.multimedia[4].url+">";
+				articleinsertion += '<div class="abstractpart"><div class="gradient"><p>'+value.abstract+'</p></div></div></div></a></li>';
 				// This is an absolute mess of appending im not sure how i would want to do this in a cleaner fashion
 
 				
 			});
-			$('.article-section').append(url+image+text);
+			$('.article-section').append(articleinsertion);
 			$('.article-wrapper').slideDown(1000)
 
 		})
