fc5e0e5989109b1b862498a1b3a723419fb51c19
Ringo <ringong95@gmail.com>
2016-10-25 14:54:44 -0700

    ES6

--- js/main.js
+++ js/main.js
@@ -1,13 +1,13 @@
 $( document ).ready(function() {
 	
-		$('#dropdown').heapbox({'onChange':function(){
+	$('#dropdown').heapbox({'onChange':()=>{
 		
-			$('.main').addClass('shrunkheader');
-			$('.loader').show();	
+		$('.main').addClass('shrunkheader');
+		$('.loader').show();	
 			$('#dropdown option[value=selction').remove();// this removes the selection option from the html dom thing
 			$("#dropdown").heapbox("update"); //Updates heapbox to fit the change i just made to the selection it is based on the html dom and effect the dom dom
-			var input = $('select').val()
-			var url = 'https://api.nytimes.com/svc/topstories/v2/'+input+'.json';
+			let input = $('select').val()
+			let url = 'https://api.nytimes.com/svc/topstories/v2/'+input+'.json';
 			$('.article-section').empty();
 			$('.article-wrapper').hide();
 			url += '?' + $.param({
@@ -18,35 +18,45 @@ $( document ).ready(function() {
 				method: 'GET',
 			})
 
-			.done(function(data){
+			.done((data)=>{
 
-			var info = [];
-			// the filter function here lets it dig through the array and keep only the hinted ones and put that into the varrible Info
-			info = $(data.results).filter(function(key,value){
+				let filteredstory = [];
+			// the filter function here lets it dig through the array and keep only the hinted ones and put that into the letrible filteredstory
+			filteredstory = $(data.results).filter(function(key,value){
 				return $(value.multimedia).length>=5;
 				// it "returns" back only ones that fulfill that requirement
 			})
-			info.splice (12);
+			filteredstory.splice (12);
 			// this i over thought so much it just cuts out everything past the 12th one
+// (key,value) 
+			let articleinsertion = ""
+			for(const{url,multimedia,abstract} of filteredstory){
 
-			var articleinsertion = ""
-			$.each(info, function (key,value) {
-				articleinsertion += '<li><a href='+value.url+">";
-				articleinsertion += '<div class=article style=background-image:url('+value.multimedia[4].url+">";
-				articleinsertion += '<div class="abstractpart"><div class="gradient"><p>'+value.abstract+'</p></div></div></div></a></li>';
-				// This is an absolute mess of appending im not sure how i would want to do this in a cleaner fashion
+				articleinsertion += 
+				`<li>
+					<a href="${url}">
+				 		<div class=article style="background-image:url(${multimedia[4].url}">
+							<div class="abstractpart"> 
+								<div class="gradient"> 
+									<p> ${abstract}</p>
+								</div>
+							</div>
+						</div>
+					</a>
+				</li>`;
+				
 
 				
-			});
+			}
 			$('.article-section').append(articleinsertion);
 			$('.article-wrapper').slideDown(1000)
 
 		})
 
-		.always(function(){
-			$('.loader').hide();	
-		});
+			.always(()=>{
+				$('.loader').hide();	
+			});
 
-	}
-});
+		}
+	});
 });	
